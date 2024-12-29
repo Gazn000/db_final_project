4f50a8e387baefa9b9d4e1b177fd1e3526b9d0f7
Gazn000 <106374728+Gazn000@users.noreply.github.com>
2024-12-29 18:15:29 +0800

    Update alzheimers.sql

--- sql/alzheimers.sql
+++ sql/alzheimers.sql
@@ -1,21 +1,7 @@
 CREATE TABLE alzheimers_diagnosed (
     PatientID INT PRIMARY KEY,
-    Age INT,
-    Gender TINYINT,
-    Ethnicity TINYINT,
-    EducationLevel TINYINT,
-    BMI FLOAT,
-    Smoking TINYINT,
     AlcoholConsumption FLOAT,
-    PhysicalActivity FLOAT,
-    DietQuality FLOAT,
-    SleepQuality FLOAT,
-    FamilyHistoryAlzheimers TINYINT,
-    CardiovascularDisease FLOAT,
-    Diabetes FLOAT,
-    Depression TINYINT,
     HeadInjury TINYINT,
-    Hypertension TINYINT,
     Diagnosis TINYINT,
     MMSE INT
 );
@@ -30,7 +16,7 @@ FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(PatientID, Age, Gender, Ethnicity, EducationLevel, BMI, Smoking, AlcoholConsumption, PhysicalActivity, DietQuality, SleepQuality, FamilyHistoryAlzheimers, @CardiovascularDisease, @Diabetes, Depression, HeadInjury, Hypertension, @SystolicBP, @DiastolicBP, @CholesterolTotal, @CholesterolLDL, @CholesterolHDL, @CholesterolTriglycerides, MMSE, @FunctionalAssessment, @MemoryComplaints, @BehavioralProblems, @ADL, @Confusion, @Disorientation, @PersonalityChanges, @DifficultyCompletingTasks, @Forgetfulness, Diagnosis, @DoctorInCharge);
+(PatientID, @Age, @Gender, @Ethnicity, @EducationLevel, @BMI, @Smoking, AlcoholConsumption, @PhysicalActivity, @DietQuality, @SleepQuality, @FamilyHistoryAlzheimers, @CardiovascularDisease, @Diabetes, @Depression, HeadInjury, @Hypertension, @SystolicBP, @DiastolicBP, @CholesterolTotal, @CholesterolLDL, @CholesterolHDL, @CholesterolTriglycerides, MMSE, @FunctionalAssessment, @MemoryComplaints, @BehavioralProblems, @ADL, @Confusion, @Disorientation, @PersonalityChanges, @DifficultyCompletingTasks, @Forgetfulness, Diagnosis, @DoctorInCharge);
 
 -- Step 2: 篩選數據到診斷表
 INSERT INTO alzheimers_diagnosed
